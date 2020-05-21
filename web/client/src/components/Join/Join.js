9c86a03b042bd5484ef4a342ae6f3536744a95b3
Narawit Tubtimtoe <elesrubsoap_4822@hotmail.com>
2020-05-21 18:52:57 +0700

    UI: change color

--- web/client/src/components/Join/Join.js
+++ web/client/src/components/Join/Join.js
@@ -1,29 +1,47 @@
-import React,{useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import './Join.css';
+import "./Join.css";
 
 const Join = () => {
-    const [name,setName] = useState('');
-    const [room ,setRoom] = useState('');
-    
-    return(
-     <div className="joinOuterContainer" >
-         
-            <div className="joinInnerContainer">
-            <div className="card">
-            <h1 className="heading" >Join  with Friend</h1>
-            <div><input placeholder="Name" className="joinInput" type="text" onChange={(event)=>setName(event.target.value)}/></div>
-            <div><input placeholder="Room" className="joinInput mt-30" type="text"  size="50" onChange={(event)=>setRoom(event.target.value)}/></div>
-            <Link onClick={event =>(!name||!room)?event.preventDefault() :null} to={`/chat?name=${name}&room=${room}`}>
-           <button className="button mt-30"  type="submit">Sign In</button>
-         
-            </Link>
-            </div>
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <div className="card">
+          <h1 className="heading">Join with Friend</h1>
+          <div>
+            <input
+              placeholder="Name"
+              className="joinInput"
+              type="text"
+              onChange={(event) => setName(event.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              placeholder="Room"
+              className="joinInput mt-30"
+              type="text"
+              size="50"
+              onChange={(event) => setRoom(event.target.value)}
+            />
+          </div>
+          <Link
+            onClick={(event) =>
+              !name || !room ? event.preventDefault() : null
+            }
+            to={`/chat?name=${name}&room=${room}`}
+          >
+            <button className="button mt-30" type="submit">
+              Sign In
+            </button>
+          </Link>
         </div>
-       
+      </div>
     </div>
-        
-    )
-}
+  );
+};
 export default Join;
--- web/client/src/components/Messages/Message/Message.js
+++ web/client/src/components/Messages/Message/Message.js
@@ -1,38 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import './Message.css';
+import "./Message.css";
 
-
-import ReactEmoji from 'react-emoji';
+import ReactEmoji from "react-emoji";
 
 const Message = ({ message: { text, user }, name }) => {
   let isSentByCurrentUser = false;
 
   const trimmedName = name.trim().toLowerCase();
+  let isSentByBot = user === "BOT" ? true : false;
 
-  if(user === trimmedName) {
+  if (user === trimmedName) {
     isSentByCurrentUser = true;
   }
 
-  return (
-    isSentByCurrentUser
-      ? (
-        <div className="messageContainer justifyEnd">
-          <p className="sentText pr-10">{trimmedName}</p>
-          <div className="messageBox backgroundBlue">
-            <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
-          </div>
-        </div>
-        )
-        : (
-          <div className="messageContainer justifyStart">
-            <div className="messageBox backgroundLight">
-              <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
-            </div>
-            <p className="sentText pl-10 " >{user}</p>
-          </div>
-        )
+  return isSentByCurrentUser ? (
+    <div className="messageContainer justifyEnd">
+      <p className="sentText pr-10">{trimmedName}</p>
+      <div className="messageBox backgroundCurrentUserColor">
+        <p className="messageText fontCurrentUserColor">
+          {ReactEmoji.emojify(text)}
+        </p>
+      </div>
+    </div>
+  ) : isSentByBot ? (
+    <div className="messageContainer justifyStart">
+      <div className="messageBox backgroundBotColor">
+        <p className="messageText fontBotColor">{ReactEmoji.emojify(text)}</p>
+      </div>
+      <p className="sentText pl-10 ">{user}</p>
+    </div>
+  ) : (
+    <div className="messageContainer justifyStart">
+      <div className="messageBox backgroundOtherColor">
+        <p className="messageText fontOtherUserColor">
+          {ReactEmoji.emojify(text)}
+        </p>
+      </div>
+      <p className="sentText pl-10 ">{user}</p>
+    </div>
   );
-}
+};
 
 export default Message;
