f93ff26ef0ad02229fdc78866615101548ecb10f
Narawit Tubtimtoe <elesrubsoap_4822@hotmail.com>
2020-05-21 13:38:26 +0700

    Bugfix: show people online

--- web/client/src/components/TextContainer/TextContainer.js
+++ web/client/src/components/TextContainer/TextContainer.js
@@ -1,35 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import currentIcon from '../../icons/currentIcon.svg';
-import onlineIcon from '../../icons/people.svg';
-import './TextContainer.css';
+import currentIcon from "../../icons/currentIcon.svg";
+import onlineIcon from "../../icons/people.svg";
+import "./TextContainer.css";
 
-const TextContainer = ({ users }) => (
-  <div className="textContainer">
-    
-    {
-      users
-        ? (
-          <div>
-            <div className="card">
+const TextContainer = ({ users }) => {
+  return (
+    <div className="textContainer">
+      {users ? (
+        <div>
+          <div className="card">
             <img className="people" src={onlineIcon} alt="online icon" />
             <h3>People online</h3>
             <div className="activeContainer mt-20">
               <div>
-                {users.map(({name}) => (
-                  <div key={name} className="activeItem">
-                     <img className="currentIcon"alt="Current Icon" src={currentIcon} />
-                      {name}
+                {users.map(({ username }) => (
+                  <div key={username} className="activeItem">
+                    <img
+                      className="currentIcon"
+                      alt="Current Icon"
+                      src={currentIcon}
+                    />
+                    {username}
                   </div>
                 ))}
               </div>
             </div>
           </div>
         </div>
-        )
-        : null
-    }
-  </div>
-);
+      ) : null}
+    </div>
+  );
+};
 
 export default TextContainer;
